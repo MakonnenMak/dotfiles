a225544e7860630af1ccd80b57bd9876158fd483
MakonnenMak <makonnenmako@gmail.com>
2022-02-05 19:29:41 -0500

    Update script with comments

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -1,68 +1,85 @@
 #!/bin/sh
 
+# These are files that exist in my dotfiles directory 
+# on my machine. These are symlinked to actual locations.
+
+# Specify a new file here, seperating a new configuration 
+# with a new line. Will prob move to own file if
+# this gets too big...
+
+#OPTIONS=(
+#[0]=".vimrc"
+#[1]=".zshrc"
+#...
+#[9]=".newconfig" <-- example of new addition
+#)
+
+OPTIONS=(
+[0]="sync_dotfiles.sh"
+[1]=".vimrc"
+[2]=".zshrc"
+)
+
+# Useful styling constants and helper funcs 
 YELLOW_TEXT_BLUE_BG="\e[1;33;4;44m"
 END_COLOR_MODIF="\e[0m"
+
+MAIN_PROMPT="Specify the files you wanna add in a space delimited format"
+HELPER="Example: '0 1' for vimrc and zshrc"
 
 echoWithYellowBgBlueText(){
 	echo -e ${YELLOW_TEXT_BLUE_BG}$@${END_COLOR_MODIF}
 }
-MAIN_PROMPT="Specify the files you wanna add in a space delimited format"
+
+# Script starts 
 
 echoWithYellowBgBlueText ${MAIN_PROMPT}
+echo ${HELPER}
 
-declare -A OPTIONS
-OPTIONS=(
-"vim"
-"zsh"
-)
-
-OPTION_FILE_LOCATIONS=(
-[0]=".vimrc"
-[1]=".zshrc"
-)
-
-configs=()
-displayAndGetOptions()
+userChoices=()
+setUserChoices()
 {
 	echo "Your options"
-	for i in "${!OPTION_FILE_LOCATIONS[@]}"
+	for i in "${!OPTIONS[@]}"
 	do
-	  echo "$i) ${OPTION_FILE_LOCATIONS[$i]}"
+	  echo "$i) ${OPTIONS[$i]}"
 	done
 
-	read -a configs
+	read -a userChoices
 }
+
+# Let's me choose files  
+setUserChoices
 
 echoOptions() {
 	echoWithYellowBgBlueText "You chose to add"
 	for value in ${@}
 	do
-		echo "${OPTION_FILE_LOCATIONS[${value}]}"
+		echo "${OPTIONS[${value}]}"
 	done
 }
 
-displayAndGetOptions
-
-
+# Confirm choices. Continue if correct, otherwise call the set function again
 while true; do
-    echoOptions "${configs[@]}"
+    echoOptions "${userChoices[@]}"
     read -p "Are these the correct choice? Type y for yes, n for no: "
 
-    if [[ "$REPLY" == "y" ]];           then 
+    if (( "$REPLY" == "y" ));           then 
 	    echoWithYellowBgBlueText "Great! Let's move on..." 
 	    break
     elif (( REPLY == "n"   ));           then 
 	    echoWithYellowBgBlueText 'Let us try again then...' >&2
-	    displayAndGetOptions
+	    setUserChoices 
     else
 	    echo 'Not a valid input.' >&2;
     fi
 done
 
-files=("sync_dotfiles.sh")
-for i in "${configs[@]}"
+# Now that choices are confirmed can add files, commit, and push
+files=()
+for i in ${userChoices[@]}
 do
-	files+=("${OPTION_FILE_LOCATIONS[${i}]}")
+	files+=("${OPTIONS[${i}]}")
 done
 
 echo "Adding files..."
